c54b29d91298399d159402780dd940eb3c8e79f7
PraveenKarthikeyan406 <praveenkarthikeyan406@gmail.com>
2025-06-19 15:59:04 +0530

    second

---
+++ pract.js
@@ -0,0 +1,29 @@
+let a=10;
+let name="PK",b= null,c
+let isbool=true,d=1234567898765432145678n
+
+console.log(a,name,b,c,isbool,d);
+// operators
+console.log(5+5,5-4,5*5,5/3,5%2,5**3,5==5,5>3,5<3,5==='5',5===5,5!=4,5!==5,3<4 && 4>2,!true)
+//continonal statement
+/* if(condition){
+}else if(condition){
+}else{
+}*/
+
+switch(a){
+case 1:
+    console.log("One");
+    break;
+case 5:
+    console.log("Five");
+    break;
+
+case 10:
+    console.log("Ten");
+    break;
+default:
+    console.log("None");
+    break;
+}
+
